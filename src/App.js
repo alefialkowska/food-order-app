3de8f9d650744c7aafe2c617b63af1a6e059deef
Aleksandra Fiałkowska <ale.fialkowska@gmail.com>
2022-08-01 16:17:47 +0200

    Create content in Meals Folder. Create MealsInfo component with styles.module.css file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './style/App.css'
 import Header from './components/Layout/Header'
+import MealsInfo from './components/Meals/MealsInfo'
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <MealsInfo />
    
     </div>
   );
---
+++ src/components/Meals/MealsInfo.js
@@ -0,0 +1,12 @@
+import classes from './MealsInfo.module.css'
+const MealsDescription = () => {
+    return ( 
+        <div className={classes.info}>
+            <h1>Wybierz na co masz ochotę, a my to do Ciebie dostarczymy!</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis illum odio blanditiis consectetur pariatur. Quia fugiat provident veniam natus optio quas deleniti rem perferendis, non aliquid praesentium aut ipsa?</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus delectus distinctio inventore culpa. Fuga repellendus eum, in enim ullam possimus, nulla rerum accusantium perferendis blanditiis repellat qui quisquam inventore alias?</p>
+        </div>
+     );
+}
+ 
+export default MealsDescription;
