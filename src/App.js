d980b728bb9c541f88d5795a61df230789e8f041
Aleksandra Fiałkowska <ale.fialkowska@gmail.com>
2022-08-01 15:32:27 +0200

    Create Header component, add Barlow font and Header.img to assets folder

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './style/App.css'
+import Header from './components/Layout/Header'
 
 function App() {
   return (
     <div className="App">
+      <Header />
    
     </div>
   );
---
+++ src/components/Layout/Header.js
@@ -0,0 +1,18 @@
+import {Fragment} from 'react'
+import HeaderImage from './HeaderImage'
+import classes from './Header.module.css'
+import HeaderCartButton from './HeaderCartButton'
+
+const Header = () => {
+    return ( 
+        <Fragment>
+            <header className={classes.image}>
+                <h1>Restauracja</h1>
+                <HeaderCartButton /> 
+            </header>
+            <HeaderImage />  
+        </Fragment>
+    );
+}
+ 
+export default Header;
