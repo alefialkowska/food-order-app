191df8d0eea6fc6db111a5dbfd51d9216a33ab7a
Aleksandra Fiałkowska <ale.fialkowska@gmail.com>
2022-08-01 22:53:29 +0200

    Fixing formatting error

--- src/components/Meals/MealAddForm.js
+++ src/components/Meals/MealAddForm.js
@@ -1,22 +1,12 @@
 import classes from './MealAddForm.module.css'
 
 const MealAddForm = () => {
-    return ( <
-        form className = {
-            classes.form
-        } >
-        <
-        label className = {
-            classes.label
-        }
-        htmlFor = 'input' > ilość < /label> <
-        input type = 'number'
-        name = 'input'
-        className = {
-            classes.input
-        } > < /input> <
-        button > +dodaj < /button> <
-        /form>
+    return (
+        <form className={classes.form}>
+            <label htmlFor="input" className={classes.input}></label>
+            <input type="number" name='input' className={classes.input}/>
+            <button className={classes.button}>+ dodaj</button>
+        </form>
     );
 }
 
