77058d4d88d6ae403124f3f199e262ca5eb6fccb
Aleksandra Fiałkowska <ale.fialkowska@gmail.com>
2022-08-02 16:13:42 +0200

    Create CartContex and CartProvider

---
+++ src/components/store/CartProvider.js
@@ -0,0 +1,11 @@
+import CartContext from "./cartContext";
+
+const CartProvider = props => {
+    return (
+        <CartContext.Provider>
+            {props.children}
+        </CartContext.Provider>
+    )
+};
+
+export default CartProvider;
