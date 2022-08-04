8820bafc893a5fbec143de157c8e309b341b72ff
Aleksandra Fiałkowska <ale.fialkowska@gmail.com>
2022-08-04 11:24:23 +0200

    Add CartItem component to Cart component. Add CartItem.module.css add useCntext to Cart.js

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,11 +1,26 @@
+import { useContext } from 'react'
 import classes from './Cart.module.css'
 import CartPortal from '../UI/CartPortal'
+import CartContext from '../store/CartContext'
+import CartItem from './CartItem'
 
 const Cart = props => {
+
+const context = useContext(CartContext);
+const totalAmount = `${context.totalAmount.toFixed(2)}zł`;
+const isCartEmpty = context.items.length < 1;
+const handleAddItem = item => {};
+const handleRemoveItem = id => {};
+
 const cartMeals = (
     <ul className={classes['cart-items']}>
-        <li>Zupa</li>
-        <li>Spaghetti Bolognese</li>
+        {context.items.map( item => (
+            <CartItem 
+            key={item.id} 
+            item={item} 
+            handleRemoveItem={handleRemoveItem.bind(null, item.id)} 
+            handleAddItem={handleAddItem.bind(null, item)}/>
+        ))}
     </ul>
 )
 
@@ -14,11 +29,11 @@ const cartMeals = (
             {cartMeals}
             <div className={classes.total}>
                 <span>Kwota zamówienia</span>
-                <span>99.99zł</span>
+                <span>{totalAmount}</span>
             </div>
             <div className={classes.actions}>
                 <button onClick={props.hideCart} className={classes['button--alt']}>zamknij</button>
-                <button className={classes.button}>zamów</button>
+                {!isCartEmpty && <button className={classes.button}>zamów</button>}
                 </div>
         </CartPortal>
      );
