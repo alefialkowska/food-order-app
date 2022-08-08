2f0e55cd3186ac9e665486d3694e60dc78d44084
Aleksandra Fiałkowska <ale.fialkowska@gmail.com>
2022-08-08 23:13:33 +0200

    adding bump and change color animation to to Cartbutton in HeaderCartButton.js

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,14 +1,32 @@
-import { Fragment, useContext} from 'react';
+import { Fragment, useContext, useState, useEffect} from 'react';
 import CartContext from '../store/CartContext';
 import CartIcon from '../Cart/CartIcon';
 import classes from './HeaderCartButton.module.css';
 
 const HeaderCartButton = ({showCart}) => {
 
+    const [bumpActive, setBumpActive] = useState(false);
+
     const ctx = useContext(CartContext);
+
     const numberOfItems = ctx.items.reduce(
         (currentNumber, item) => currentNumber + item.amount 
         , 0)
+
+    const btnClasses = `${classes['cart-content']} ${bumpActive ? classes.bump : null}`
+
+    const {totalAmount} = ctx;
+
+    useEffect(() => {
+        if (totalAmount === 0) {
+            return
+        }
+        setBumpActive(true);
+        const timer = setTimeout(() => {
+                setBumpActive(false)
+            }, 500);
+        return () => {clearTimeout(timer)}
+    }, [totalAmount]);
 
     return (
         <Fragment>
@@ -17,7 +35,7 @@ const HeaderCartButton = ({showCart}) => {
                 <CartIcon classes={classes}/>
             </div>
             <div>Twój koszyk</div>
-            <div className={classes['cart-content']}>{numberOfItems}</div>
+            <div className={btnClasses}>{numberOfItems}</div>
         </button>
         </Fragment>
     );
