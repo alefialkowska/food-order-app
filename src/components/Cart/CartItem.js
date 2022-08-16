3c68f3323a71e1cfea061a88292cff0c910b8118
Aleksandra Fiałkowska <ale.fialkowska@gmail.com>
2022-08-16 09:25:21 +0200

    Fix error after merge

---
+++ src/components/Cart/CartItem.js
@@ -0,0 +1,24 @@
+import classes from './CartItem.module.css'
+
+const CartItem = ({item, handleRemoveItem, handleAddItem}) => {
+
+    const itemPrice = `${item.price.toFixed(2)}zł`;
+
+    return ( 
+        <li className={classes['cart-item']}>
+      <div>hghmg
+        <h2>{item.name}</h2>
+        <div className={classes.summary}>
+          <span className={classes.price}>{itemPrice}</span>
+          <span className={classes.amount}>x {item.amount}</span>
+        </div>
+      </div>
+      <div className={classes.actions}>
+        <button onClick={handleRemoveItem}>−</button>
+        <button onClick={handleAddItem}>+</button>
+      </div>
+    </li>
+     );
+}
+ 
+export default CartItem;
