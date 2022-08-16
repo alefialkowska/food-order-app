870e062f549a0381b1f86ecc1694064c97642343
Aleksandra Fiałkowska <ale.fialkowska@gmail.com>
2022-08-16 10:26:22 +0200

    add state to Cart component to handle confirm button and conditionally show checkout form and action buttons

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,4 +1,4 @@
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 import classes from './Cart.module.css'
 import CartPortal from '../UI/CartPortal'
 import CartContext from '../store/CartContext'
@@ -7,27 +7,39 @@ import Checkout from './Checkout'
 
 const Cart = props => {
 
-const context = useContext(CartContext);
-const totalAmount = `${context.totalAmount.toFixed(2)}zł`;
-const isCartEmpty = context.items.length < 1;
-const handleAddItem = item => {
-    context.addItem({...item, amount: 1})
-};
-const handleRemoveItem = id => {
-    context.removeItem(id)
-};
+    const [isFormActive, setIsFormActive] = useState(false);
 
-const cartMeals = (
-    <ul className={classes['cart-items']}>
-        {context.items.map( item => (
-            <CartItem 
-            key={item.id} 
-            item={item} 
-            handleRemoveItem={handleRemoveItem.bind(null, item.id)} 
-            handleAddItem={handleAddItem.bind(null, item)}/>
-        ))}
-    </ul>
-)
+    const context = useContext(CartContext);
+    const totalAmount = `${context.totalAmount.toFixed(2)}zł`;
+    const isCartEmpty = context.items.length < 1;
+    const handleAddItem = item => {
+        context.addItem({...item, amount: 1})
+    };
+    const handleRemoveItem = id => {
+        context.removeItem(id)
+    };
+    const handleOrderButton = () => {
+        setIsFormActive(true)
+    };
+
+    const cartMeals = (
+        <ul className={classes['cart-items']}>
+            {context.items.map( item => (
+                <CartItem 
+                key={item.id} 
+                item={item} 
+                handleRemoveItem={handleRemoveItem.bind(null, item.id)} 
+                handleAddItem={handleAddItem.bind(null, item)}/>
+            ))}
+        </ul>
+    )
+    
+    const actions = (
+    <div className={classes.actions}>
+    <button onClick={props.hideCart} className={classes['button--alt']}>zamknij</button>
+    {!isCartEmpty && <button className={classes.button} onClick={handleOrderButton}>zamów</button>}
+    </div>
+    );
 
     return ( 
         <CartPortal hideCart={props.hideCart}>
@@ -36,11 +48,9 @@ const cartMeals = (
                 <span>Kwota zamówienia</span>
                 <span>{totalAmount}</span>
             </div>
-            <Checkout/>
-            <div className={classes.actions}>
-                <button onClick={props.hideCart} className={classes['button--alt']}>zamknij</button>
-                {!isCartEmpty && <button className={classes.button}>zamów</button>}
-                </div>
+            {isFormActive && <Checkout/>}
+            {!isFormActive && actions}
+            
         </CartPortal>
      );
 }
